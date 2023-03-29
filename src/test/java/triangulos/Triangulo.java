e697ea9a2b9fb651dd90649b8e62c913beea010a
Higor Gonçalves <101122887+Higor-OGoncaves@users.noreply.github.com>
2023-03-29 10:27:06 -0300

    Update Triangulo.java

--- src/test/java/triangulos/Triangulo.java
+++ src/test/java/triangulos/Triangulo.java
@@ -6,21 +6,21 @@ public class Triangulo {
 		if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
 			return "escaleno";
 		} else {
-			return "nada";
+			return "ERRO";
 		}
 	}
 	public String calc1(int ladoE1, int ladoE2, int ladoE3) {
 		if (ladoE1 == ladoE2 && ladoE2 == ladoE1 &&  ladoE1 == ladoE3) {
 			return "equilatero";
 		} else {
-			return "nada";
+			return "ERRO";
 		}
 	}
 	public String calc2(int ladoI1, int ladoI2, int ladoI3) {
 		if (ladoI1 != ladoI2 && ladoI2 == ladoI3) {
 			return "isosceles";
 		} else {
-			return "nada";
+			return "ERRO";
 		}
 	}
 }
